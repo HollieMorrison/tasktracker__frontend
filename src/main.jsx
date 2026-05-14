62b92c4908f00f97355354047999bdfdd415eca4
HollieMorrison <104519409+HollieMorrison@users.noreply.github.com>
2026-05-14 21:35:35 +0100

    Fix mobile navbar toggle

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <BrowserRouter>  
-        <App />
-     </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
